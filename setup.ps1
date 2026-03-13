0e3c1c0a9982a9def17ad8c5967af58c22258885
seyukun <yus-sato@kalyte.ro>
2026-03-14 03:18:38 +0900

    Refactor setup script to use variables for image and VHDX names

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,13 @@
 #!/usr/sbin/env -S pwsh.exe
 
-if (-Not (Test-Path ".\data\ubuntu-24.04-server-cloudimg-amd64.vhdx")) {
-    if (-Not (Test-Path ".\data\ubuntu-24.04-server-cloudimg-amd64.img")) {
-        curl -sLO "https://ftp.udx.icscoe.jp/Linux/ubuntu-cloud-images/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img"
+$IMG_NAME = "ubuntu-24.04-server-cloudimg-amd64.img"
+$VHDX_NAME = "ubuntu-24.04-server-cloudimg-amd64.vhdx"
+
+if (-Not (Test-Path ".\data\$VHDX_NAME")) {
+    if (-Not (Test-Path ".\data\$IMG_NAME")) {
+        curl -sLo ".\data\$IMG_NAME" "https://ftp.udx.icscoe.jp/Linux/ubuntu-cloud-images/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img" 
     }
-    echo "qemu-img convert -p -f qcow2 -O vhdx ./data/ubuntu-24.04-server-cloudimg-amd64.img ./data/ubuntu-24.04-server-cloudimg-amd64.vhdx" | wsl .
+    echo "qemu-img convert -p -f qcow2 -O vhdx ./data/$IMG_NAME ./data/$VHDX_NAME" | wsl .
 }
 
 echo "cloud-localds ./data/seed.iso ./data/user-data.yml ./data/meta-data.yml" | wsl .
